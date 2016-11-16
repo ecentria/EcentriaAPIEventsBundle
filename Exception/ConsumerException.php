00429f47832d228e14a4991d0b1fb68c5b08276e
Dmytro Bazavluk <dmitriy.bazavluk@opticsplanet.com>
2016-11-16 11:40:45 -0600

    OMP-2743 extracting response logic into ResponseException

--- Exception/ConsumerException.php
+++ Exception/ConsumerException.php
@@ -9,120 +9,13 @@
  */
 
 namespace Ecentria\Libraries\EcentriaAPIEventsBundle\Exception;
-use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
 
 /**
- * Abstract Flow Exception
+ * Generic consumer exception
  *
  * @copyright   2016 OpticsPlanet, Inc
  * @author      Eugene Boiarynov <ievgen.boiarynov@opticsplanet.com>
  */
 class ConsumerException extends \Exception
 {
-    /**
-     * Message status flag
-     *
-     * @var int
-     */
-    private $flag;
-
-    /**
-     * Whether this exception should stop consumer
-     *
-     * @var bool
-     */
-    private $stopConsuming;
-
-    /**
-     * Constructor
-     *
-     * @param string $flag          One of the message flag defined
-     *                              inside \OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface
-     * @param bool   $stopConsuming Whether it is necessary to stop the consumer or not
-     */
-    public function __construct($flag, $stopConsuming = false)
-    {
-        if (
-            !in_array(
-                $flag,
-                [
-                    ConsumerInterface::MSG_ACK,
-                    ConsumerInterface::MSG_REJECT,
-                    ConsumerInterface::MSG_REJECT_REQUEUE,
-                    ConsumerInterface::MSG_SINGLE_NACK_REQUEUE
-                ]
-            )
-        ) {
-            throw new \InvalidArgumentException(
-                sprintf('RabbitMQ ConsumerException can\'t get %s flag as a parameter', $flag)
-            );
-        }
-        $this->flag = $flag;
-        $this->stopConsuming = $stopConsuming;
-    }
-
-    /**
-     * Creates exception with ConsumerInterface::MSG_ACK flag
-     *
-     * @param bool $stopConsuming Whether it is necessary to stop the consumer or not
-     *
-     * @return ConsumerException
-     */
-    public static function ack($stopConsuming = false) {
-        return new self(ConsumerInterface::MSG_ACK, $stopConsuming);
-    }
-
-
-    /**
-     * Creates exception with ConsumerInterface::MSG_REJECT flag
-     *
-     * @param bool $stopConsuming Whether it is necessary to stop the consumer or not
-     *
-     * @return ConsumerException
-     */
-    public static function reject($stopConsuming = false) {
-        return new self(ConsumerInterface::MSG_REJECT, $stopConsuming);
-    }
-
-
-    /**
-     * Creates exception with ConsumerInterface::MSG_REJECT_REQUEUE flag
-     *
-     * @param bool $stopConsuming Whether it is necessary to stop the consumer or not
-     *
-     * @return ConsumerException
-     */
-    public static function rejectAndRequeue($stopConsuming = false) {
-        return new self(ConsumerInterface::MSG_REJECT_REQUEUE, $stopConsuming);
-    }
-
-
-    /**
-     * Creates exception with ConsumerInterface::MSG_SINGLE_NACK_REQUEUE flag
-     *
-     * @param bool $stopConsuming Whether it is necessary to stop the consumer or not
-     *
-     * @return ConsumerException
-     */
-    public static function nackAndRequeue($stopConsuming = false) {
-        return new self(ConsumerInterface::MSG_SINGLE_NACK_REQUEUE, $stopConsuming);
-    }
-
-    /**
-     * Returns a flag value
-     *
-     * @return int
-     */
-    public function getFlag() {
-        return $this->flag;
-    }
-
-    /**
-     * Returns true if it is necessary to stop the consumer, false otherwise
-     *
-     * @return bool
-     */
-    public function stopConsuming() {
-        return $this->stopConsuming;
-    }
 }
---
+++ Exception/ResponseException.php
@@ -0,0 +1,132 @@
+<?php
+/*
+ * This file is part of the ecentria group, inc. software.
+ *
+ * (c) 2016, ecentria group, inc.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Ecentria\Libraries\EcentriaAPIEventsBundle\Exception;
+use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
+
+/**
+ * Exception to deliver extensive response from the listener
+ *
+ * Encapsulates information:
+ * - acknowledge status flag if default ACK doesn't work for particular case
+ * - whether consumer should stop its execution
+ *
+ * @copyright   2016 OpticsPlanet, Inc
+ * @author      Dmytro Bazavluk <dmitriy.bazavluk@opticsplanet.com>
+ */
+class ResponseException extends ConsumerException
+{
+    /**
+     * Message status flag
+     *
+     * @var int
+     */
+    private $flag;
+
+    /**
+     * Whether this exception should stop consumer
+     *
+     * @var bool
+     */
+    private $stopConsuming;
+
+    /**
+     * Constructor
+     *
+     * @param string $flag          One of the message flag defined
+     *                              inside \OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface
+     * @param bool   $stopConsuming Whether it is necessary to stop the consumer or not
+     */
+    public function __construct($flag, $stopConsuming = false)
+    {
+        if (
+            !in_array(
+                $flag,
+                [
+                    ConsumerInterface::MSG_ACK,
+                    ConsumerInterface::MSG_REJECT,
+                    ConsumerInterface::MSG_REJECT_REQUEUE,
+                    ConsumerInterface::MSG_SINGLE_NACK_REQUEUE
+                ]
+            )
+        ) {
+            throw new \InvalidArgumentException(
+                sprintf('RabbitMQ ConsumerException can\'t get %s flag as a parameter', $flag)
+            );
+        }
+        $this->flag = $flag;
+        $this->stopConsuming = $stopConsuming;
+    }
+
+    /**
+     * Creates exception with ConsumerInterface::MSG_ACK flag
+     *
+     * @param bool $stopConsuming Whether it is necessary to stop the consumer or not
+     *
+     * @return ConsumerException
+     */
+    public static function createAck($stopConsuming = false) {
+        return new self(ConsumerInterface::MSG_ACK, $stopConsuming);
+    }
+
+
+    /**
+     * Creates exception with ConsumerInterface::MSG_REJECT flag
+     *
+     * @param bool $stopConsuming Whether it is necessary to stop the consumer or not
+     *
+     * @return ConsumerException
+     */
+    public static function createReject($stopConsuming = false) {
+        return new self(ConsumerInterface::MSG_REJECT, $stopConsuming);
+    }
+
+
+    /**
+     * Creates exception with ConsumerInterface::MSG_REJECT_REQUEUE flag
+     *
+     * @param bool $stopConsuming Whether it is necessary to stop the consumer or not
+     *
+     * @return ConsumerException
+     */
+    public static function createdRejectAndRequeue($stopConsuming = false) {
+        return new self(ConsumerInterface::MSG_REJECT_REQUEUE, $stopConsuming);
+    }
+
+
+    /**
+     * Creates exception with ConsumerInterface::MSG_SINGLE_NACK_REQUEUE flag
+     *
+     * @param bool $stopConsuming Whether it is necessary to stop the consumer or not
+     *
+     * @return ConsumerException
+     */
+    public static function createNackAndRequeue($stopConsuming = false) {
+        return new self(ConsumerInterface::MSG_SINGLE_NACK_REQUEUE, $stopConsuming);
+    }
+
+    /**
+     * Returns a flag value
+     *
+     * @return int
+     */
+    public function getFlag() {
+        return $this->flag;
+    }
+
+    /**
+     * Returns true if it is necessary to stop the consumer, false otherwise
+     *
+     * @return bool
+     */
+    public function stopConsuming() {
+        return $this->stopConsuming;
+    }
+}
