15a96cb81aef258d9d6517d2c7ce368d0e3a3601
Oleg Andreyev <oleg.andreyev@ecentria.com>
2020-12-16 19:23:15 +0200

    EXP-5463 - remove file comment

--- tests/Integration/App/AppKernel.php
+++ tests/Integration/App/AppKernel.php
@@ -1,13 +1,5 @@
 <?php
 declare(strict_types=1);
-/*
- * This file is part of the eCORE CART software.
- *
- * (c) 2020 ecentria, Inc
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 
 namespace Ecentria\Libraries\EcentriaAPIEventsBundle\Tests\Integration\App;
 
